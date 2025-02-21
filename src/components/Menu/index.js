13a8bf7178fcc98f2ed797383d03db351305063d
Jackcubbi <jackcubbi@gmail.com>
2025-02-21 22:12:13 +0200

    edit: menu render

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,4 +3,17 @@ import menuItem from "./item";
 
 export default class Menu {
   //  render - Display the menu on the page
+  static render() {
+    config.wp
+      .pages()
+      .parent(0)
+      .order("asc")
+      .orderby("menu_order")
+      .then((pages) => {
+        let menuPages = pages.map((page) => menuItem.render(page));
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+      });
+  }
 }
