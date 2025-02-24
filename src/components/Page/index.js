49173c1e20beaad6410deb6c5b975ce61230c944
Jackcubbi <jackcubbi@gmail.com>
2025-02-24 14:51:26 +0200

    add: Render pages on the page

---
+++ src/components/Page/index.js
@@ -0,0 +1,25 @@
+import config from "../../lib/config";
+import Helpers from "../../lib/Helpers";
+
+export default class Page {
+  /**
+   * Render pages on the page
+   */
+  static render(slug) {
+    config.wp,
+      pages()
+        .slug(slug)
+        .embed()
+        .perPage(1)
+        .then((pages) => {
+          if (pages[0]) {
+            Helpers.renderContent(pages[0]);
+          } else {
+            Helpers.renderContent(config.page404);
+          }
+        })
+        .catch((err) => {
+          console.log(`Error: ${err}`);
+        });
+  }
+}
