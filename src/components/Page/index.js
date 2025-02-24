9840a2ab63bd90167da26ef4ea9ef94491cb8cc8
Jackcubbi <jackcubbi@gmail.com>
2025-02-24 16:04:51 +0200

    fix: config.wp , to .

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -6,20 +6,20 @@ export default class Page {
    * Render pages on the page
    */
   static render(slug) {
-    config.wp,
-      pages()
-        .slug(slug)
-        .embed()
-        .perPage(1)
-        .then((pages) => {
-          if (pages[0]) {
-            Helpers.renderContent(pages[0]);
-          } else {
-            Helpers.renderContent(config.page404);
-          }
-        })
-        .catch((err) => {
-          console.log(`Error: ${err}`);
-        });
+    config.wp
+      .pages()
+      .slug(slug)
+      .embed()
+      .perPage(1)
+      .then((pages) => {
+        if (pages[0]) {
+          Helpers.renderContent(pages[0]);
+        } else {
+          Helpers.renderContent(config.page404);
+        }
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+      });
   }
 }
