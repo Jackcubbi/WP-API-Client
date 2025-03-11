ddefef05f3d795623663aa3369deff1641349574
Jackcubbi <jackcubbi@gmail.com>
2025-03-11 14:15:21 +0200

    add: comments to single post

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,5 +1,6 @@
 import config from "../../lib/config";
 import Helpers from "../../lib/Helpers";
+import Comments from "../Comments/index";
 
 export default class Post {
   /**
@@ -12,6 +13,7 @@ export default class Post {
       .embed()
       .then((post) => {
         Helpers.renderContent(post[0], "h1", false);
+        Comments.render(post[0].id);
       })
       .catch((err) => {
         console.log("Error: " + err);
