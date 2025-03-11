1e4756f4391d528cdd0dc0eb481910fe0cf11642
Jackcubbi <jackcubbi@gmail.com>
2025-03-11 14:21:28 +0200

    add: posts on the post

---
+++ src/components/Categories/Category.js
@@ -0,0 +1,22 @@
+import config from "../../lib/config";
+import Helpers from "../../lib/Helpers";
+
+export default class Category {
+  /*
+   * render a posts on the post
+   */
+  static render(catID = "") {
+    config.wp
+      .posts()
+      .embed()
+      .categories(catID)
+      .then((posts) => {
+        posts.forEach((post) => {
+          Helpers.renderContent(post, "h2", true);
+        });
+      })
+      .catch((err) => {
+        console.log("Error: " + err);
+      });
+  }
+}
