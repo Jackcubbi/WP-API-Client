9ad1bdc43750c5d4b9e4b6387b0eff591fe3da51
Jackcubbi <jackcubbi@gmail.com>
2025-03-12 13:21:33 +0200

    add: render tags in a list sidebar

---
+++ src/components/Widgets/Tag/index.js
@@ -0,0 +1,35 @@
+import config from "../../../lib/config";
+import Category from "./Tag";
+
+export default class Tags {
+  /*
+   * Render tags list in a list
+   */
+  static render() {
+    if (document.querySelector("ul.tags")) {
+      return false;
+    }
+
+    config.wp
+      .tags()
+      .then((tags) => {
+        const sidebar = config.sidebar,
+          widget = document.createElement("div");
+
+        widget.classList.add("widget");
+        widget.innerHTML = `
+      <h3>Tags</h3>
+      <ul class='tags'></ul>
+    `;
+
+        sidebar.appendChild(widget);
+
+        categories.forEach((tag) => {
+          Category.render(tag);
+        });
+      })
+      .catch((err) => {
+        console.log("Error: " + err);
+      });
+  }
+}
