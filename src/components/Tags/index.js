62a5ab80fda024416db7359e8a88ca78bc0a0a88
Jackcubbi <jackcubbi@gmail.com>
2025-03-12 14:05:10 +0200

    add: Render posts on the post based Tag

---
+++ src/components/Tags/index.js
@@ -0,0 +1,26 @@
+import config from "../../lib/config";
+import Helpers from "../../lib/Helpers";
+import Tag from "./Tag";
+
+export default class Tags {
+  /**
+   * Render posts on the post
+   */
+  static async render(tagSlug = "") {
+    try {
+      const tags = await config.wp.tags().embed().slug(tagSlug);
+
+      if (!tags.length) {
+        console.log(`No tags found for slug: ${tagSlug}`);
+        return;
+      }
+
+      tags.map(({ id, name, count }) => {
+        Helpers.renderHeader(`Tag: ${name} [${count}]`);
+        Tag.render(id);
+      });
+    } catch (err) {
+      console.error("Error fetching tags:", err);
+    }
+  }
+}
