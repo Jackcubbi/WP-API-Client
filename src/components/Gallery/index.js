c76250c406b35a54c2e72633c29b8787bd2b0347
Jackcubbi <jackcubbi@gmail.com>
2025-03-13 15:40:20 +0200

    edit: main corrections

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -6,17 +6,18 @@ export default class Gallery {
    * render Gallery to page
    */
   static render() {
-    const galleryContainer = document.createElement("ul");
-    galleryContainer.classList.add("gallery");
-    config.articleContainer.appendChild(galleryContainer);
-
     config.wp
       .media()
       .then((images) => {
-        images.forEach((image) => image.render(image));
+        let galleryContainer = document.createElement("ul");
+
+        galleryContainer.classList.add("gallery");
+        config.articleContainer.appendChild(galleryContainer);
+
+        images.forEach((image) => Image.render(image));
       })
       .catch((err) => {
-        console.error("Error:", err);
+        console.log("Error: " + err);
       });
   }
 }
